c141d03b5417477cb75675d1ed75b4970c85f8d7
dihiay <dihiasalhi@gmail.com>
2022-06-14 19:10:09 +0200

    login/logout

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from .views import  TaskList, TaskCreate,TaskDetail, TaskUpdate, TaskDelete
+from .views import  TaskList, TaskCreate,TaskDetail, TaskUpdate, TaskDelete, CustomLoginView
+from django.contrib.auth.views import LogoutView 
 
 urlpatterns = [
+
+path('logout/', LogoutView.as_view(next_page = 'login'), name='logout'),
+
+path('login/', CustomLoginView.as_view(), name='login'),
 
 path('task/<int:pk>/',TaskDetail.as_view(),name = "task"),
 
@@ -16,6 +21,6 @@ path('task-delete/<int:pk>/',TaskDelete.as_view(),name = "task-delete"),
 #we put an umpty string for the first argument so this url will be our base url
 #for the 2nd argument we can't use a class directly, so we calla function inside the class 
 #the 3rd argument is the name of the view in the url 'url pattern'
-    path('',TaskList.as_view(), name="tasks"),
-    path('task-create',TaskCreate.as_view(), name="task-create"),
+path('',TaskList.as_view(), name="tasks"),
+path('task-create',TaskCreate.as_view(), name="task-create"),
 ]
