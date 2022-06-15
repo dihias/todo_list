1d2679d1e65581276a07743de583b70dac432555
dihiay <dihiasalhi@gmail.com>
2022-06-15 16:43:57 +0200

    users specific data

--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.views.generic.list import ListView
 
@@ -38,6 +39,12 @@ class TaskList(LoginRequiredMixin,ListView):
    # to replace "object_list" with "tasks" in the template
    context_object_name = 'tasks'
 
+   def get_context_data(self, **kwargs):
+    context =super().get_context_data(**kwargs)
+    context['tasks'] = context['tasks'].filter(user=self.request.user)
+    context['count'] = context['tasks'].filter(complete=False).count()
+    return context
+
 
 
 class TaskCreate(LoginRequiredMixin,CreateView):
@@ -46,12 +53,18 @@ class TaskCreate(LoginRequiredMixin,CreateView):
     #we could choose some items in the field : field= ['title', 'description']
 
     #if we wan list out all of the items inthefield we could just do as fellow:
-    fields = '__all__'
+    fields = ['title','description','complete']
+
+    success_url= reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
 
     #we put the url name as an attribute, so it redirects to that url
 class TaskUpdate(LoginRequiredMixin,UpdateView):
     model = Task 
-    fields= '__all__'
+    fields= ['title','description','complete']
     success_url = reverse_lazy('tasks')
 
 
